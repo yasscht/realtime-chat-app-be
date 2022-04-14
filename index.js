eaea94cb7788116ff814985cc42acb3d4eb4e9c3
yassine.chtiaa@etu.uae.ac.ma <yassine.chtiaa@etu.uae.ac.ma>
2022-04-14 12:53:39 +0000

    controllers setup and auth route building

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const authRoutes = require("./routes/auth.js");
 require("dotenv").config();
 app.use(cors());
 app.use(express.json());
@@ -9,4 +10,5 @@ app.use(express.urlencoded());
 app.get("/", (req, res) => {
   res.send("hello world");
 });
+app.use("/auth", authRoutes);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
